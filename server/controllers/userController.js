f8ee9c4f9416219e7ee9211a9da811624b4c31cc
2177meetpatel <patelmeet21771906@gmail.com>
2026-07-10 21:52:08 +0530

    Complete authentication module

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+export const getProfile = (req, res) => {
+    try {
+
+        res.status(200).json({
+            success: true,
+            message: "Profile Fetched Successfully",
+            user: req.user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
+// Load environment variables
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
-  res.send("FleetDash Backend is Running 🚀");
+  res.status(200).json({
+    success: true,
+    message: "🚀 FleetDash Backend API is Running Successfully",
+  });
 });
 
+// Server Port
 const PORT = process.env.PORT || 5000;
 
+// Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
