6b0ea1ae53edd12c87cd160111e8d22814988cb6
2177meetpatel <patelmeet21771906@gmail.com>
2026-09-22 17:36:54 +0530

    fix: configure server for deployment

--- server/server.js
+++ server/server.js
@@ -69,9 +69,9 @@ const startServer = async () => {
   const PORT = process.env.PORT || 5000;
 
   // Start Server
-  app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
-  });
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
 };
 
 startServer();
